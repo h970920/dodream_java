18a9468c7ee333c92ea874ef7f0f7fd08cb643a6
h970920 <h970920@naver.com>
2023-12-19 22:53:59 +0900

    복습

--- java_basic/src/hyun_woo/day_01_re.java
+++ java_basic/src/hyun_woo/day_01_re.java
@@ -3,8 +3,21 @@ package hyun_woo;
 public class day_01_re {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-
+    
+    for ( int i=0; i < 5; i++) 
+    {
+      System.out.print("*");
+        for ( int a=0; a < i; a++) 
+        {
+          System.out.print("*");
+          
+        }
+            System.out.println();
+    }
+    
+    
   }
-
-}
+  
+  
+  }
+    
