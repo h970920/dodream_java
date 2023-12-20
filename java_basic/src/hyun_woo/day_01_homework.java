3bfd4e2d2d2564769195747e2c85a5603cd1abcb
h970920 <h970920@naver.com>
2023-12-20 17:56:46 +0900

    23_1220

---
+++ java_basic/src/hyun_woo/day_01_homework.java
@@ -0,0 +1,47 @@
+package hyun_woo;
+/*
+ * 현우가 내준 숙제 해보기
+ */
+
+
+public class day_01_homework {
+
+public static void main(String[] args) {
+   
+  
+    for ( int a = 0; a < 5; a++) {
+      for ( int b = 0 ; b < 5; b++) {
+        System.out.print("o");
+      }
+          System.out.println();
+        for ( int c = 5; a < c; c--) {
+          System.out.print("x");
+        }
+    }   System.out.println();
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
