44ef619aa7f5314275682517eea151a6878f3a88
h970920 <h970920@naver.com>
2023-12-21 20:21:13 +0900

    2023_12_21

--- java_basic/src/hyun_woo/day_01_homework.java
+++ java_basic/src/hyun_woo/day_01_homework.java
@@ -9,15 +9,30 @@ public class day_01_homework {
 public static void main(String[] args) {
    
   
-    for ( int a = 0; a < 5; a++) {
-      for ( int b = 0 ; b < 5; b++) {
-        System.out.print("o");
-      }
-          System.out.println();
-        for ( int c = 5; a < c; c--) {
-          System.out.print("x");
-        }
-    }   System.out.println();
+   for ( int i=1; i<=5; i++) {
+     for( int b=4; b>=i; b--) {
+       System.out.print("x");
+     }  for (int s=1; s<=i; s++ ) {
+       System.out.print("o");
+     }    for (int m=2; m<=i; m++) {
+       System.out.print("o");
+     }
+         System.out.println();
+   }
+   for( int i=1; i<=5; i++) {
+     for(int b=1; b<=i; b++) {
+       System.out.print("x");
+     }
+       for(int c=0; c<7-i; c++) {
+         System.out.print("o");
+       }
+         for (int x=2; x>=i+1; x--) {
+           System.out.print("o");
+         }
+         System.out.println();
+         }
+           
+   }
   
   
   
@@ -29,9 +44,7 @@ public static void main(String[] args) {
   
   
   
-  
-  
-}  
+ 
   
   
   
---
+++ java_basic/src/hyun_woo/day_02_rp.java
@@ -0,0 +1,27 @@
+package hyun_woo;
+/*
+ * 역피라미드만들기
+ */
+
+public class day_02_rp {
+public static void main(String[] args) {
+      
+   
+   for(int i=0; i<=6; i++ ) {
+     for(int s=0; s<i+1; s++) {
+       System.out.print("*");
+     }  for(int b=0; b<i+1; b++) {
+         System.out.print("o");
+     }
+       System.out.println();
+   }
+  
+  
+  
+  
+  
+  
+}
+  
+  
+}
